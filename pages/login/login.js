65b633a7eee4d4617db73bb79218bf9884c65ecf
Mr Hong <857329255@qq.com>
2019-11-25 18:14:30 +0800

    2019年11月25日 First Commit

---
+++ pages/login/login.js
@@ -0,0 +1,32 @@
+// pages/login.js 
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    
+  },
+
+  /**
+   * 微信登录事件 ES5
+   */
+  wxLoginAction: function() {
+
+  },
+  /**
+   * 豆瓣登录事件 ES5
+   */
+  doubanLoginAction: function() {
+
+  },
+  /**
+   * 底部协议事件 ES6函数写法
+   */
+  procotolAction() {
+    wx.navigateTo({
+      // 从根目录开始
+      url: '/pages/agreement/agreement'
+    });
+  }
+})
