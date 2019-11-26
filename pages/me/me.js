9e50519a168122548c0ad791460ce82c926572d9
Mr Hong <857329255@qq.com>
2019-11-26 18:08:46 +0800

    2019年11月26日

--- pages/me/me.js
+++ pages/me/me.js
@@ -5,62 +5,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    myBookVideoes: [
+      {
+        flag: 0,
+        imageSrc: "/assets/images/me/me_movie",
+        subtitle: "观影分析",
+        readCount: 0,
+        flagCount: 10,
+      }
+    ]
   },
 
   /**
-   * 生命周期函数--监听页面加载
+   * 登录事件 ES6函数写法
    */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  loginAction() {
+    wx.navigateTo({
+      url: '/pages/login/login',
+    }); 
   }
 })
