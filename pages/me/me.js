f0787f42bc7eb0b20bf759712c3cc335df00a849
Mr Hong <857329255@qq.com>
2019-11-27 18:04:32 +0800

    2019年11月27日

--- pages/me/me.js
+++ pages/me/me.js
@@ -7,12 +7,26 @@ Page({
   data: {
     myBookVideoes: [
       {
-        flag: 0,
-        imageSrc: "/assets/images/me/me_movie",
+        id: 0,
+        imageSrc: "/assets/images/me/me_movie.png",
         subtitle: "观影分析",
         readCount: 0,
-        flagCount: 10,
-      }
+        flagDesc: "标记10部影片\n开启观影分析",
+      },
+      {
+        id: 1,
+        imageSrc: "/assets/images/me/me_book.png",
+        subtitle: "读书分析",
+        readCount: 0,
+        flagDesc: "标记10本书\n开启读书分析",
+      },
+      {
+        id: 2,
+        imageSrc: "/assets/images/me/me_music.png",
+        subtitle: "音乐分析",
+        readCount: 0,
+        flagDesc: "标记10张唱片\n开启音乐分析",
+      },
     ]
   },
 
@@ -23,5 +37,20 @@ Page({
     wx.navigateTo({
       url: '/pages/login/login',
     }); 
+  },
+
+  /**
+   * 立即开启事件 ES6函数写法
+   */
+  startAction(event) {
+    let model = event.currentTarget.dataset.model
+    
+    if (model.id == 0) {
+      console.log('观影分析');
+    } else if (model.id == 1) {
+      console.log('读书分析');
+    } else {
+      console.log('音乐分析');
+    }
   }
 })
